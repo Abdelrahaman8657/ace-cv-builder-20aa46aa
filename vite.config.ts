a420ca79cd554bbbfc7eb0636a0c050d3be9347a
Abdelrahaman8657 <body132547698@gmail.com>
2026-01-02 13:49:38 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Portfolio/", // ضيف السطر ده باسم المستودع بتاعك بين علامتين /
+  base: "/ace-cv-builder/", // لازم تحط اسم المستودع بتاعك بالظبط هنا
   server: {
     host: "::",
     port: 8080,
